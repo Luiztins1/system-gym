ae89194e8c9249f171f8341060cbaa692d601c07
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-06-26 18:11:53 -0300

    refactor: add DuplicateException and NotFoundException in GlobalHandlerException.

--- src/main/java/com/github/Luiztins1/controller/common/GlobalExceptionHandler.java
+++ src/main/java/com/github/Luiztins1/controller/common/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.github.Luiztins1.controller.common;
 
 import com.github.Luiztins1.controller.dtos.FieldErrorDTO;
 import com.github.Luiztins1.controller.dtos.ResponseErrorDTO;
+import com.github.Luiztins1.exceptions.DuplicateException;
+import com.github.Luiztins1.exceptions.NotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -30,5 +33,23 @@ public class GlobalExceptionHandler {
                         .map(error -> new FieldErrorDTO(error.getField(), error.getDefaultMessage()))
                         .toList();
         return new ResponseErrorDTO(HttpStatus.BAD_REQUEST, "Campos com valores inválidos ou vazios.", fieldErrorDTOList);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseErrorDTO dataIntegrityViolationException (DataIntegrityViolationException e){
+        return new ResponseErrorDTO(HttpStatus.CONFLICT, "Erro de integridade ou restrição de banco de dados.", List.of());
+    }
+
+    @ExceptionHandler(DuplicateException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseErrorDTO duplicateException (DuplicateException e){
+        return new ResponseErrorDTO(HttpStatus.CONFLICT, e.getMessage(), List.of());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseErrorDTO notFoundException (NotFoundException e){
+        return new ResponseErrorDTO(HttpStatus.NOT_FOUND, e.getMessage(), List.of());
     }
 }
