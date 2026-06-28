8e641c57fd6020ef756d73fc2c161718b61b6612
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-06-28 19:46:37 -0300

    feat: created Bean Validation in UserAuthDTO.

--- src/main/java/com/github/Luiztins1/controller/dtos/UserAuthDTO.java
+++ src/main/java/com/github/Luiztins1/controller/dtos/UserAuthDTO.java
@@ -1,9 +1,18 @@
 package com.github.Luiztins1.controller.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record UserAuthDTO(
+
+        @NotNull(message = "Preencha o campo corretamente.")
         String login,
+
+        @NotNull(message = "Preencha o campo corretamente.")
         String password,
+
+        @NotBlank(message = "Adicione corretamente as roles.")
         List<String> roles) {
 }
