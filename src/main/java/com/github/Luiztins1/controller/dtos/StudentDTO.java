1a0ae2175974f58ccd2ba0b3b421f9677c415b4a
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-06-29 14:31:13 -0300

    refactor: use @CPF annotation in StudentDTO for Bean Validator.

--- src/main/java/com/github/Luiztins1/controller/dtos/StudentDTO.java
+++ src/main/java/com/github/Luiztins1/controller/dtos/StudentDTO.java
@@ -2,6 +2,7 @@ package com.github.Luiztins1.controller.dtos;
 
 import com.github.Luiztins1.model.enums.TypeModality;
 import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -13,12 +14,12 @@ public record StudentDTO(
         @NotBlank(message = "Preencha o campo corretamente.")
         String name,
 
-        @Size(min = 11, max = 11)
+        @CPF
         @NotBlank(message = "Preencha o campo corretamente")
         String cpf,
 
         @Size(min = 6, max = 128)
-        @NotBlank(message = "Preench o campo corretamente.")
+        @NotBlank(message = "Preencha o campo corretamente.")
         String address,
 
         @NotNull(message = "Preencha o campo corretamente.")
