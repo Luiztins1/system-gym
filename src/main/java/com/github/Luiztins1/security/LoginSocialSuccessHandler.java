0606c19b342c72bfbc7ebf7ce97c3d66eeb6c009
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-07-19 14:38:38 -0300

    feat: created SecurityService.

--- src/main/java/com/github/Luiztins1/security/LoginSocialSuccessHandler.java
+++ src/main/java/com/github/Luiztins1/security/LoginSocialSuccessHandler.java
@@ -25,7 +25,7 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
             HttpServletResponse response,
             Authentication authentication) throws ServletException, IOException {
 
-        
+
 
         super.onAuthenticationSuccess(request, response, authentication);
     }
